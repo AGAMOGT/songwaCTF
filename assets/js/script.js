53584826be7f94f6995e735a8239163b57fb3f4a
AGAMOGT <daffay93@gmail.com>
2025-10-16 08:44:05 +0700

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Pindahkan semua JavaScript dari index.html ke sini
+function createMatrixRain() {
+    // ... matrix rain code
+}
+
+function showLogin() {
+    // ... login function
+}
+
+// ... semua fungsi JavaScript
